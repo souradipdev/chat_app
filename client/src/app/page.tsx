6e15bfd1cc95397888b32daa69eae9ba61780260
souradipdev <22052939@kiit.ac.in>
2024-12-17 21:01:39 +0530

    Initialised chat UI

---
+++ client/src/app/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Page() {
+  return (
+    <div className={"bg-accent text-accent-foreground h-screen w-screen dark"}>Home</div>
+  );
+}
+
+export default Page;
